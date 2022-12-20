e5cc19b3a5424af6ccc59a758038c0971b733c53
mohabrd <bezrhoudimohamed@gmail.com>
2022-12-20 12:31:01 +0100

    3

--- public/js/main.js
+++ public/js/main.js
@@ -5,3 +5,12 @@ let prenom = prompt("Entrez votre prénom");
 if(prenom.length < 5){
     alert("Ton nom est trop court");
 }
+
+let nombre1 = Math.round(Math.random()*10);
+let nombre2 = Math.round(Math.random()*10);
+let addition = prompt(`${nombre1} + ${nombre2} =`);
+if(nombre1 + nombre2 == addition){
+    alert("Félicitations");
+} else{
+    alert("C'est incorrect");
+}
